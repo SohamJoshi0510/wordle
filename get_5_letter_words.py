b110886e11093a7d9d2dede5805dede9c1476a1f
SohamJoshi0510 <132748258+SohamJoshi0510@users.noreply.github.com>
2026-01-18 22:40:21 +0530

    Update filtering method

--- get_5_letter_words.py
+++ get_5_letter_words.py
@@ -1,3 +1,3 @@
 with open('words_alpha.txt', 'r') as fileObj:
-    words = filter(lambda x: len(x) == 6, fileObj.readlines())
-    word_list = list(map(lambda x: x.strip('\n'), words))
+    word_list = [line.rstrip() for line in fileObj if len(line) == 6]
+    print(word_list)
